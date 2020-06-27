3517d418eabf765868bec4c109c8671dff4e8a2d
james <jamesvincent.ocariza@bpoc.co.jp>
2020-06-27 12:33:46 +0800

    added minifier and change some file structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,60 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const {
+    src,
+    dest,
+    parallel
+} = require('gulp');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const postcss = require('gulp-postcss');
+const terser = require('gulp-terser');
 const browserSync = require('browser-sync').create();
 
-function style(){
+const jsPath = "./js/**/*.js";
+const cssPath = "./css/**/*.css";
+
+function style() {
     return gulp.src('./scss/**/*.scss')
-    .pipe(sass().on('error',sass.logError))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/assets/css'))
+        .pipe(browserSync.stream());
 }
 
-function watch(){
+function imgmin() {
+    return src('assets/img/*').pipe(imagemin()).pipe(dest('dist/images'));
+}
+
+function cssMinify() {
+    return src(cssPath)
+        .pipe(sourcemaps.init())
+        .pipe(concat('style.css'))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('dist/assets/css'));
+}
+
+
+function watch() {
     browserSync.init({
-        server:{
+        server: {
             baseDir: './'
         }
     });
 
-    gulp.watch('./scss/**/*.scss',style);
+    gulp.watch('./scss/**/*.scss', style);
     gulp.watch('./*.html').on('change', browserSync.reload);
     gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
 exports.style = style;
 exports.watch = watch;
+exports.imgmin = imgmin;
+exports.cssMinify = cssMinify;
+exports.default = parallel(imgmin, cssMinify);
