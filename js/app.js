616488583a3614884875e85729ac7ed353eda03a
CatMeowlet <jameszzz999888@gmail.com>
2020-07-08 11:50:30 +0800

    revise color and styles to dark theme

---
+++ js/app.js
@@ -0,0 +1,96 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 'slow');
+}
+
+
+// $("#btn-s-product").click(function() {
+//   $('html, body').animate({
+//       scrollTop: $("#s-product").offset().top
+//   }, 1000);
+// });
+// handle links with @href started with '#' only
+$(document).on('click', 'a[href^="#"]', function (e) {
+  // target element id
+  var id = $(this).attr('href');
+
+  // target element
+  var $id = $(id);
+  if ($id.length === 0) {
+    return;
+  }
+
+  // prevent standard hash navigation (avoid blinking in IE)
+  e.preventDefault();
+
+  // top position relative to the document
+  var pos = $id.offset().top;
+
+  // animated top scrolling
+  $('body, html').animate({
+    scrollTop: pos
+  });
+});
+
+
+// $('.faq-box').on('click', function () {
+//   var $this = $(this);
+//     $(this).find('.faq-sign-none, .faq-sign-display').toggle();
+
+//     if ( $(this).hasClass("active")) {
+//       $(this).find(".faq-answer").slideUp();
+//       $(this).removeClass("active");
+//     } else {
+//       $(this).find(".faq-answer").slideDown();
+//       $(this).addClass("active");
+//     }
+//   });
+$('.faq_container').on('click', function () {
+  var $this = $(this);
+  $(this).find('.faq_up-arrow, .faq_down-arrow').toggle();
+  if ($(this).hasClass("active")) {
+    $(this).find(".answer").slideUp();
+    $(this).removeClass("active");
+  } else {
+    $(this).find(".answer").slideDown();
+    $(this).addClass("active");
+  }
+});
